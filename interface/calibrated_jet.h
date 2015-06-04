157686cdb5454115d83f58d46ec4ca721d49a805
Aashish <helloworld@aashishtripathee.com>
2015-06-04 15:13:44 -0400

    Calibrated Jet

---
+++ interface/calibrated_jet.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip> 
+#include <sstream>
+#include <ostream>
+#include <stdexcept>
+#include <cmath>
+
+#include "fastjet/ClusterSequence.hh"
+
+
+namespace MOD {
+   class CalibratedJet {
+
+      public:
+         CalibratedJet(double px, double py, double pz, double energy, string algorithm);
+         CalibratedJet(std::istringstream & input_stream);
+         CalibratedJet();
+
+         fastjet::PseudoJet pseudojet() const;
+         std::string make_string() const;
+         std::string make_header_string() const;
+
+         friend std::ostream& operator<< (std::ostream&, const CalibratedJet&);
+
+      private:
+         std::string _algorithm;
+         fastjet::PseudoJet _pseudojet;
+   };
+}
