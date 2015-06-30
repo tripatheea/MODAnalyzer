7a7651556f310ee007e4b99a20f9d43d0c89d706
Aashish <helloworld@aashishtripathee.com>
2015-06-30 17:43:03 -0400

    Python

--- python/plots.py
+++ python/plots.py
@@ -18,23 +18,19 @@ def parse_file(input_file):
   # Hardest_pT Corr_Hardest_pT Prescale Trigger_Name zg_05 zg_1 zg_2
   
   properties = defaultdict(list)
-  '''
-  hardest_pts = []
-  hardest_corrected_pts = []
-  prescales = []
-  trigger_names = []
-  zg_05s = []
-  zg_1s = []
-  zg_2s = []
-  '''
 
   for line in lines:
     try:
       numbers = line.split()
       
       if not numbers[0] == "#":
-        properties['hardest_pts'].append( float( numbers[3] ) )
+        properties['uncorrected_hardest_pts'].append( float( numbers[3] ) )
+        properties['corrected_hardest_pts'].append( float( numbers[4] ) )
         properties['prescales'].append( int( numbers[5] ) )
+        properties['trigger_names'].append(  numbers[6] )
+        properties['zg_05'].append( float( numbers[7] ) )
+        properties['zg_1'].append( float( numbers[8] ) )
+        properties['zg_2'].append( float( numbers[9] ) )
 
     except:
       pass
@@ -43,25 +39,35 @@ def parse_file(input_file):
     
 
 
+def plot_pts(pTs, prescales):
+
+  plt.hist(np.array(pTs), 200, normed=1, weights=prescales, facecolor='red', alpha=1.0)
+
+  plt.autoscale(True)
+  # plt.xlim(0, 1000)
+
+  plt.xlabel('$p_T$ (GeV)')
+  plt.grid(True)
+  plt.yscale('log', nonposy='clip')
+
+  plt.show()
+
+
+def plot_zg(zgs, prescales):
+
+  plt.hist(np.array(zgs), 50, normed=1, weights=prescales, facecolor='red', alpha=1.0)
+
+  plt.autoscale(True)
+  plt.xlim(0, 0.6)
+
+  plt.xlabel('$z_g$')
+  plt.grid(True)
+
+  plt.show()
+
 
 properties = parse_file(input_analysis_file)
 
+plot_pts(properties['uncorrected_hardest_pts'], properties['prescales'])
 
-print properties['hardest_pts']
-
-
-x = np.array(properties['hardest_pts'])
-
-
-# the histogram of the data
-n, bins, patches = plt.hist(x, 50, normed=1, weights=properties['prescales'], facecolor='red', alpha=0.75)
-
-# plt.axis([0, 1500, 0, 0.03])
-plt.autoscale(True)
-
-plt.xlabel('Smarts')
-plt.ylabel('Probability')
-plt.grid(True)
-plt.yscale('log', nonposy='clip')
-
-plt.show()
+# plot_zg(properties['zg_05'], properties['prescales'])
