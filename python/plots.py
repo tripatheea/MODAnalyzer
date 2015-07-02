36bb5af2e033fe2199b46e5025c884c899c8ca4c
Aashish <helloworld@aashishtripathee.com>
2015-07-02 01:17:25 -0400

    Plots

--- python/plots.py
+++ python/plots.py
@@ -11,7 +11,7 @@ input_analysis_file = sys.argv[1]
 
 
 
-def parse_file(input_file):
+def parse_file(input_file, pT_lower_cut = 0.00):
   f = open(input_file, 'r')
   lines = f.read().split("\n")
 
@@ -24,13 +24,14 @@ def parse_file(input_file):
       numbers = line.split()
       
       if not numbers[0] == "#":
-        properties['uncorrected_hardest_pts'].append( float( numbers[3] ) )
-        properties['corrected_hardest_pts'].append( float( numbers[4] ) )
-        properties['prescales'].append( int( numbers[5] ) )
-        properties['trigger_names'].append(  numbers[6] )
-        properties['zg_05'].append( float( numbers[7] ) )
-        properties['zg_1'].append( float( numbers[8] ) )
-        properties['zg_2'].append( float( numbers[9] ) )
+        if float(numbers[3]) > pT_lower_cut:
+          properties['uncorrected_hardest_pts'].append( float( numbers[3] ) )
+          properties['corrected_hardest_pts'].append( float( numbers[4] ) )
+          properties['prescales'].append( int( numbers[5] ) )
+          properties['trigger_names'].append(  numbers[6] )
+          properties['zg_05'].append( float( numbers[7] ) )
+          properties['zg_1'].append( float( numbers[8] ) )
+          properties['zg_2'].append( float( numbers[9] ) )
 
     except:
       pass
@@ -39,40 +40,52 @@ def parse_file(input_file):
     
 
 
-def plot_pts(pTs, corrected_pTs, prescales):
+def plot_pts():
+  properties = parse_file(input_analysis_file)
 
-  plt.hist(np.array(pTs), 500, normed=1, weights=prescales, log=1, histtype='step', color='blue') 
-  plt.hist(np.array(corrected_pTs), 500, normed=1, weights=prescales, log=1, histtype='step', color='red') 
+  pTs = properties['uncorrected_hardest_pts']
+  corrected_pTs = properties['corrected_hardest_pts']
+  prescales = properties['prescales']
 
+  plt.hist(np.array(pTs), 500, normed=1, label="Uncorrected", weights=prescales, log=1, histtype='step', color='blue') 
+  plt.hist(np.array(corrected_pTs), 500, normed=1, label="Corrected", weights=prescales, log=1, histtype='step', color='red') 
 
   plt.autoscale(True)
   plt.xlim(0, 2000)
+
+  plt.legend()
   plt.xlabel('$p_T$ (GeV)')
   plt.grid(True)
 
   plt.show()
 
 
-def plot_zg(zgs, prescales):
+def plot_zg():
+  properties = parse_file(input_analysis_file, pT_lower_cut = 153)
+
+  zgs = [properties['zg_05'], properties['zg_1'], properties['zg_2']]
+  prescales = properties['prescales']
 
   colors = ['red', 'blue', 'green']
+  labels = ['0.05', '0.1', '0.2']
 
   i = 0
   for zg in zgs:
-    plt.hist(np.array(zg), 100, normed=1, weights=prescales, facecolor=colors[i], histtype='step')
+    plt.hist(np.array(zg), 100, normed=1, label=labels[i], weights=prescales, facecolor=colors[i], histtype='step')
     i += 1
 
   plt.autoscale(True)
   plt.xlim(0, 0.6)
 
+  plt.legend()
   plt.xlabel('$z_g$')
   plt.grid(True)
 
   plt.show()
 
 
-properties = parse_file(input_analysis_file)
 
-# plot_pts(properties['uncorrected_hardest_pts'], properties['corrected_hardest_pts'], properties['prescales'])
 
-plot_zg([properties['zg_05'], properties['zg_1'], properties['zg_2']], properties['prescales'])
+plot_pts()
+
+plot_zg()
