2D plot normalization fix

python/2d_plots.py
@@ -94,7 +94,7 @@ default_dir = "plots/Version 6/"
 
 
 logo_location = "/home/aashish/root/macros/MODAnalyzer/mod_logo.png"
-logo_text = "v1.1"
+logo_text = "v1.2"
 
 
 
@@ -182,9 +182,9 @@ def two_dim_plots():
 				H = np.flipud(H)
 				Hmasked = np.ma.masked_where(H == 0, H) # Mask pixels with a value of zero
 
-				plt.pcolor(xedges,yedges, Hmasked, cmap=color, vmin=0, vmax=10)
+				plt.pcolor(xedges,yedges, Hmasked, cmap=color, vmin=0, vmax=20)
 
-				cbar = plt.colorbar(ticks=[2 * i for i in range(6)])
+				cbar = plt.colorbar(ticks=[4 * i for i in range(6)])
 				# cbar.ax.set_ylabel('$\\frac{1}{\sigma} \\frac{\mathrm{d}^2 \sigma}{\mathrm{d} z_g \mathrm{d} \\theta_g}$', labelpad=150, fontsize=105, rotation=0)
 				# cbar.ax.set_ylim(0, 24)
 
@@ -293,14 +293,31 @@ def two_dim_log_plots():
 				
 				np_region_x_s, np_region_y_s, np_region_z_s = [], [], []
 
+				# print 
+				total_integral = hist.integral()
+				
+				x_bounds = hist.bounds(axis=0)
+				y_bounds = hist.bounds(axis=1)
+
+				area = (math.log(x_bounds[1], math.e) - math.log(x_bounds[0], math.e)) *  (math.log(y_bounds[1], math.e) - math.log(y_bounds[0], math.e))
+
+				sum_of_bin_contents = sum([hist.GetBinContent(i, j) for i in range(1, hist.nbins(0) + 1) for j in range(1, hist.nbins(1) + 1)])
+
+				# print sum_of_bin_contents, total_integral
+
 				# for i in range(1, hist.nbins(0) + 2):
-				for i in range(1, hist.nbins(0) + 3):
+				for i in range(1, hist.nbins(0) + 1):
 					# for j in range(1, hist.nbins(1) + 2):
-					for j in range(1, hist.nbins(1) + 3):
+					for j in range(1, hist.nbins(1) + 1):
 						
-						z = hist.GetBinContent(i, j)
-						x = hist.GetXaxis().GetBinCenter(i) - (hist.GetXaxis().GetBinWidth(i) / 2)
-						y = hist.GetYaxis().GetBinCenter(j) - (hist.GetYaxis().GetBinWidth(j) / 2)
+						z = hist.nbins(0) * hist.nbins(1) * hist.GetBinContent(i, j) / ( total_integral * area )
+						# x = hist.GetXaxis().GetBinCenter(i) - (hist.GetXaxis().GetBinWidth(i) / 2)
+						# y = hist.GetYaxis().GetBinCenter(j) - (hist.GetYaxis().GetBinWidth(j) / 2)
+
+						# print z
+
+						x = hist.GetXaxis().GetBinCenter(i)
+						y = hist.GetYaxis().GetBinCenter(j)
 
 						x_s.append(x)
 						y_s.append(y)
@@ -312,8 +329,10 @@ def two_dim_log_plots():
 				
 				# print i, j
 
+				# print max(z_s)
 
-				H, xedges, yedges = np.histogram2d(x_s, y_s, bins=[np.logspace(math.log(float(0.1), math.e), math.log(0.5, math.e), 25, base=np.e), np.logspace(math.log(float(0.01), math.e), math.log(1.0, math.e), 25, base=np.e)], weights=z_s, normed=True)
+				# DO NOT USE NORMED=True HERE.
+				H, xedges, yedges = np.histogram2d(x_s, y_s, bins=[np.logspace(math.log(float(0.1), math.e), math.log(0.5, math.e), (25 + 1), base=np.e), np.logspace(math.log(float(0.01), math.e), math.log(1.0, math.e), (25 + 1), base=np.e)], weights=z_s)
 
 				H_normalized = np.array(H)
 				H = H_normalized
@@ -322,9 +341,9 @@ def two_dim_log_plots():
 				H = np.flipud(H)
 				Hmasked = np.ma.masked_where(H == 0, H) # Mask pixels with a value of zero
 
-				plt.pcolor(xedges,yedges, Hmasked, cmap=color, vmin=0, vmax=20)
+				plt.pcolor(xedges,yedges, Hmasked, cmap=color, vmin=0, vmax=0.5)
 				
-				cbar = plt.colorbar(ticks=[4 * i for i in range(6)])
+				cbar = plt.colorbar(ticks=[0.1 * i for i in range(6)])
 				cbar.ax.tick_params(labelsize=70) 
 				# cbar.ax.set_ylabel('$\\frac{z_g \\theta_g}{\sigma} \\frac{\mathrm{d}^2 \sigma}{\mathrm{d} z_g \mathrm{d} \\theta_g}$', labelpad=150, fontsize=105, rotation=0)
 
@@ -393,7 +412,7 @@ def two_dim_log_plots():
 start = time.time()
 
 
-two_dim_plots()
+# two_dim_plots()
 two_dim_log_plots()
 
 end = time.time()

python/hists.py
@@ -2152,7 +2152,7 @@ def trigger_hists():
 
 
 		additional_text = [ ( (-0.08, 0.98), 'upper left', "AK5; \left| \eta \\right| < 2.4$ \n $p_T^{\mathrm{jet}} > 85~\mathrm{GeV}$" ) ]
-		all_hists[trigger_name + "_prescale"] =  MODHist(Hist(np.logspace(math.log(float(1e-1), math.e), math.log(1e5, math.e), 100, base=np.e)), conditions=[(['jet_quality', 3], lambda x, y: y >= x), (['trig_jet_matched', 1], lambda x, y: y == x)], use_prescale=True, x_scale='log', y_scale='log', x_label="Trigger Prescale", y_label="A.U.", y_range=(0., 1.2), additional_text=additional_text )
+		all_hists[trigger_name + "_prescale"] =  MODHist(Hist(np.logspace(math.log(float(1e0), math.e), math.log(1e4, math.e), (100 + 1), base=np.e)), conditions=[(['jet_quality', 3], lambda x, y: y >= x), (['trig_jet_matched', 1], lambda x, y: y == x)], use_prescale=True, x_scale='log', y_scale='log', x_label="Trigger Prescale", y_label="A.U.", y_range=(0., 1.2), additional_text=additional_text )
 
 
 	return all_hists
@@ -2282,7 +2282,7 @@ def two_dim_log_hists():
 			
 	# zg_hist = Hist2D(50, 0.0, 0.5, 50, 0, 1.0)
 	# zg_10_hist = Hist(np.logspace(math.log(float(0.01), math.e), math.log(0.5, math.e), 50, base=np.e))
-	zg_hist = Hist2D(np.logspace(math.log(float(0.1), math.e), math.log(0.5, math.e), 26, base=np.e), np.logspace(math.log(float(0.01), math.e), math.log(1.05, math.e), 26, base=np.e))
+	zg_hist = Hist2D(np.logspace(math.log(float(0.1), math.e), math.log(0.5, math.e), (25 + 1), base=np.e), np.logspace(math.log(float(0.01), math.e), math.log(1.0, math.e), (25 + 1), base=np.e))
 
 	pT_boundaries = [85, 115, 150, 200, 250]
 

python/plots_old.py
@@ -8839,7 +8839,7 @@ def plot_2d_theta_g_zg(pT_lower_cut=150, zg_cut='0.10', zg_filename='zg_10', log
 					stuff = line.split()
 					z_g_s.append( float(stuff[0]))
 					theta_g_s.append( float(stuff[1]))
-					prescales.append( float(stuff[2]))
+					prescales.append( float(stuff[2]) )
 
 
 			# zg_hist = Hist2D(50, -1, 99, markersize=3.0, color=plot_colors['data'])
@@ -8850,8 +8850,8 @@ def plot_2d_theta_g_zg(pT_lower_cut=150, zg_cut='0.10', zg_filename='zg_10', log
 
 
 			if log:
-				zg_bins = np.logspace(math.log(float(0.02), math.e), math.log(0.5, math.e), 25, base=np.e)
-				theta_g_bins = np.logspace(math.log(float(0.02), math.e), math.log(1.0, math.e), 25, base=np.e)
+				zg_bins = np.logspace(math.log(float(0.1001), math.e), math.log(0.5001, math.e), 25 + 1, base=np.e)
+				theta_g_bins = np.logspace(math.log(float(0.01001), math.e), math.log(1.001, math.e), 25 + 1, base=np.e)
 				bins = [zg_bins, theta_g_bins]
 			else:
 				bins = [25, 25]
@@ -8865,6 +8865,8 @@ def plot_2d_theta_g_zg(pT_lower_cut=150, zg_cut='0.10', zg_filename='zg_10', log
 			x_s = []
 			y_s =[]
 			z_s = []
+
+			# print zg_hist.nbins(0), zg_hist.nbins(1)
 
 			for i in range(1, zg_hist.nbins(0) + 1):
 				for j in range(1, zg_hist.nbins(1) + 1):
@@ -8882,9 +8884,10 @@ def plot_2d_theta_g_zg(pT_lower_cut=150, zg_cut='0.10', zg_filename='zg_10', log
 			# print x_s, y_s, z_s
 
 			if log:
-				H, xedges, yedges = np.histogram2d(z_g_s, theta_g_s, bins=bins, weights=prescales, normed=True)
+				# DO NOT USE NORMED=True HERE.
+				H, xedges, yedges = np.histogram2d(z_g_s, theta_g_s, bins=bins, weights=prescales)
 			else:
-				H, xedges, yedges = np.histogram2d(x_s, y_s, bins=[25, 25], range=[[0.0, 0.5], [0.0, 1.0]], weights=z_s, normed=True)					# Use for linear.
+				H, xedges, yedges = np.histogram2d(x_s, y_s, bins=[25, 25], range=[[0.0, 0.5], [0.0, 1.0]], weights=z_s)					# Use for linear.
 			
 
 
@@ -8897,14 +8900,14 @@ def plot_2d_theta_g_zg(pT_lower_cut=150, zg_cut='0.10', zg_filename='zg_10', log
 			Hmasked = np.ma.masked_where(H == 0, H) # Mask pixels with a value of zero
 
 			if log:
-				plt.pcolor(xedges, yedges, Hmasked, cmap=colors[counter], vmin=0, vmax=20)
+				plt.pcolor(xedges, yedges, Hmasked, cmap=colors[counter], vmin=0, vmax=0.5)
 			else:
-				plt.pcolor(xedges, yedges, Hmasked, cmap=colors[counter], vmin=0, vmax=10)
+				plt.pcolor(xedges, yedges, Hmasked, cmap=colors[counter], vmin=0, vmax=20)
 
 			if log:
-				cbar = plt.colorbar(ticks=[4 * i for i in range(6)])
+				cbar = plt.colorbar(ticks=[0.1 * i for i in range(6)])
 			else:
-				cbar = plt.colorbar(ticks=[2 * i for i in range(6)])
+				cbar = plt.colorbar(ticks=[4 * i for i in range(6)])
 
 			cbar.ax.tick_params(labelsize=70) 
 
@@ -8980,7 +8983,7 @@ def plot_2d_theta_g_zg(pT_lower_cut=150, zg_cut='0.10', zg_filename='zg_10', log
 
 
 			logo_offset_image = OffsetImage(read_png(get_sample_data("/home/aashish/root/macros/MODAnalyzer/mod_logo.png", asfileobj=False)), zoom=0.25, resample=1, dpi_cor=1)
-			text_box = TextArea("v1", textprops=dict(color='#444444', fontsize=50, weight='bold'))
+			text_box = TextArea("v1.2", textprops=dict(color='#444444', fontsize=50, weight='bold'))
 			logo_and_text_box = HPacker(children=[logo_offset_image, text_box], align="center", pad=0, sep=25)
 			anchored_box = AnchoredOffsetbox(loc=2, child=logo_and_text_box, pad=0.8, frameon=False, borderpad=0., bbox_to_anchor=[0.085, 1.0], bbox_transform = plt.gcf().transFigure)
 			plt.gca().add_artist(anchored_box)